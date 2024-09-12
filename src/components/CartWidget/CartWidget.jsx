3e5531ffa80baa866c7bd05f46c04f8d63b9cebf
Aluhé García <aluhegarcia07@gmail.com>
2024-09-12 10:09:47 -0300

    remplacé la imagen del carrito por un icon svg

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./CartWidget.css"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 const CartWidget = () => {
   return (
     <div className='Carrito'>
-        <img src="https://images.vexels.com/media/users/3/132104/isolated/preview/5f2ebb95984bf47ea01319291eb81f68-icono-de-silueta-de-carrito-de-compras-by-vexels.png" alt="" />
+        <ShoppingCartIcon sx={{ fontSize: 50 }} ></ShoppingCartIcon>
         <h3>3</h3>
     </div>
   )
